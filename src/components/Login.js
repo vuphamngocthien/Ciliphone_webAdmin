496473440b49da751c63bc8be62176f9f23841c5
luanlbps16836 <luanlbps16836@fpt.edu.vn>
2022-12-16 14:20:26 +0700

    LoginPage

--- src/components/Login.js
+++ src/components/Login.js
@@ -1,12 +1,107 @@
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View, Image, TouchableOpacity } from 'react-native';
-import GetData from './src/components/DataGrid';
+import { StyleSheet, Text, View, Image, TouchableOpacity, TextInput } from 'react-native';
 import React, { useState, useEffect } from 'react';
 
-export default function Login () {
+export default function Login(props) {
 
-};
+  return (
+    <View style={styles.container}>
+      <View style={styles.formContainer}>
+        <View style={styles.reactAdminContainer}>
+          <Text style={styles.reactAdminText}>React Admin</Text>
+          <View style={styles.formInputContainer}>
+            <TextInput style={styles.inputUsername} placeholder="Username" />
+            <TextInput style={styles.inputPassword} placeholder="Password" />
+            <TouchableOpacity style={styles.btnLogin}>Sign In</TouchableOpacity>
+          </View>
+        </View>
+      </View>
+      <View style={styles.footer}>
+        <Image style={styles.githubImg} source={require('../../assets/img/github.png')} />
+        <View style={styles.language}>
+          <Text style={styles.vietnamese}>Vietnamese</Text>
+          <Text style={styles.english}>English</Text>
+          <Text style={styles.french}>French</Text>
+        </View>
+      </View>
+    </View>
+  );
+}
 
 const styles = StyleSheet.create({
-    
-})
+  container: {
+    flex: 1,
+    backgroundColor: '#fff',
+    justifyContent: 'center',
+    alignItems: 'center',
+  },
+  formContainer: {
+    width: 450,
+    height: 250,
+    backgroundColor: '#D9D9D9',
+    justifyContent: 'center',
+    alignItems: 'center',
+    borderRadius: 15
+  },
+  reactAdminContainer: {
+
+  },
+  reactAdminText: {
+    textAlign: 'center',
+    fontSize: 24,
+    fontWeight: 'bold',
+    color: '#1677FF',
+    marginBottom: 30,
+  },
+  formInputContainer: {
+    width: 300,
+    height: 150
+  },
+  inputUsername: {
+    height: 30,
+    marginBottom: 10,
+    backgroundColor: 'white',
+    borderRadius: 10,
+    textAlign: "center"
+  },
+  inputPassword: {
+    height: 30,
+    marginBottom: 10,
+    backgroundColor: 'white',
+    borderRadius: 10,
+    textAlign: "center"
+  },
+  btnLogin: {
+    width: '100%',
+    height: 30,
+    justifyContent: 'center',
+    alignItems: 'center',
+    backgroundColor: '#1677FF',
+    borderRadius: 10,
+    color: '#FFFFFF',
+    marginTop: 20
+  },
+  footer: {
+    flexDirection: 'row',
+    marginTop: 200,
+    justifyContent: 'center',
+    alignItems: 'center',
+  },
+  githubImg: {
+    height: 24,
+    width: 24,
+    marginRight: 20 
+  },
+  language: {
+    flexDirection: 'row',
+  },
+  vietnamese: {
+    marginRight: 20
+  },
+  english: {
+    marginRight: 20
+  },
+  french: {
+    marginRight: 20
+  }
+});
